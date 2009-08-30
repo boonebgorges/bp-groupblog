8a3553fc76590357dacc080b7ee2cc823c832dde
MariusOoms <MariusOoms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-30 13:04:25 +0000

    using url instead of path for enqueing
    git-svn-id: http://plugins.svn.wordpress.org/bp-groupblog/trunk@150541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-groupblog-cssjs.php
+++ bp-groupblog-cssjs.php
@@ -8,7 +8,7 @@ function bp_groupblog_add_js() {
   
 	if ( $bp->current_component == $bp->groups->slug && ( ('group-blog' == $bp->action_variables[0]) || ('group-blog' == $bp->action_variables[1]) ) )
 	  if ( file_exists( STYLESHEETPATH . '/groupblog/js/general.js' ) )
-			wp_enqueue_script( 'bp-groupblog-js', STYLESHEETPATH . '/groupblog/js/general.js' );
+			wp_enqueue_script( 'bp-groupblog-js', get_stylesheet_directory_uri() . '/groupblog/js/general.js' );
 		else
 		  wp_enqueue_script( 'bp-groupblog-js', WP_PLUGIN_URL . '/bp-groupblog/groupblog/js/general.js' );
 }
@@ -20,7 +20,7 @@ add_action( 'template_redirect', 'bp_groupblog_add_js', 1 );
 function bp_groupblog_add_screen_css() {
 
   if ( file_exists( STYLESHEETPATH . '/groupblog/css/screen.css' ) )
-  	wp_enqueue_style( 'bp-groupblog-screen', STYLESHEETPATH . '/groupblog/css/screen.css' );
+  	wp_enqueue_style( 'bp-groupblog-screen', get_stylesheet_directory_uri() . '/groupblog/css/screen.css' );
   	  else
   	wp_enqueue_style( 'bp-groupblog-screen', WP_PLUGIN_URL . '/bp-groupblog/groupblog/css/screen.css' );
 }
