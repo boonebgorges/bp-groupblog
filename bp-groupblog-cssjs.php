fb594dfd7e13eb4072aa15355ca9d1935d835ddc
Christian Wach <needle@haystack.co.uk>
2012-03-09 13:58:48 +0000

    replaced STYLESHEETPATH with get_stylesheet_directory

--- bp-groupblog-cssjs.php
+++ bp-groupblog-cssjs.php
@@ -25,7 +25,7 @@ function bp_groupblog_add_admin_style() {
  */
 function bp_groupblog_add_js() {
 	if ( bp_is_groups_component() && bp_is_action_variable( 'group-blog' ) ) {
-		if ( file_exists( STYLESHEETPATH . '/groupblog/js/general.js' ) )
+		if ( file_exists( get_stylesheet_directory() . '/groupblog/js/general.js' ) )
 			wp_enqueue_script( 'bp-groupblog-js', get_stylesheet_directory_uri() . '/groupblog/js/general.js', array( 'jquery' ) );
 		else
 			wp_enqueue_script( 'bp-groupblog-js', plugins_url() . '/bp-groupblog/groupblog/js/general.js', array( 'jquery' ) );
@@ -38,7 +38,7 @@ add_action( 'wp_enqueue_scripts', 'bp_groupblog_add_js', 1 );
  */
 function bp_groupblog_add_screen_css() {
 	if ( bp_is_groups_component() ) {
-		if ( file_exists( STYLESHEETPATH . '/groupblog/css/style.css' ) )
+		if ( file_exists( get_stylesheet_directory() . '/groupblog/css/style.css' ) )
 			wp_enqueue_style( 'bp-groupblog-screen', get_stylesheet_directory_uri() . '/groupblog/css/style.css' );
 		else
 			wp_enqueue_style( 'bp-groupblog-screen', plugins_url() . '/bp-groupblog/groupblog/css/style.css' );
