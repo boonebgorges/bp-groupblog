eb31e9794ba76a4b9b3d07fc2432b929ca4ca0bf
Boone Gorges <boone@gorg.es>
2016-11-22 10:09:19 -0600

    fix caching for get_groupblog_group_id()

--- bp-groupblog-templatetags.php
+++ bp-groupblog-templatetags.php
@@ -109,11 +109,10 @@ function groupblog_group_id( $blog_id ) {
 
 		$cached = wp_cache_get( $blog_id, 'bp_groupblog_blog_group_ids' );
 		if ( false === $cached ) {
-			if ( $row = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$bp->groups->table_name_groupmeta} WHERE meta_key = 'groupblog_blog_id' AND meta_value = %d", $blog_id ) ) ) {
-				$group_id = (int) $row->group_id;
-
-				wp_cache_set( $blog_id, $group_id, 'bp_groupblog_blog_group_ids' );
-			}
+			$group_id = (int) $wpdb->get_var( $wpdb->prepare( "SELECT group_id FROM {$bp->groups->table_name_groupmeta} WHERE meta_key = 'groupblog_blog_id' AND meta_value = %s", $blog_id ) );
+			wp_cache_set( $blog_id, $group_id, 'bp_groupblog_blog_group_ids' );
+		} else {
+			$group_id = (int) $cached;
 		}
 
 		return $group_id;
