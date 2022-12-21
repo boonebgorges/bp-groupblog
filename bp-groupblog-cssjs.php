334d530fd2f0e2f771ca4f244ea5d314de868aa9
Lena Stergatou <aqua6617@yahoo.com>
2022-12-21 17:24:12 +0200

    version 1..9.2
    = 1.9.3 =
    * Test with WordPress 6.1.1, BuddyPress 10.6 and PHP 8.1
    * Change arguments order of function groupblog_edit_base_settings()
    * Change arguments order of function bp_groupblog_get_user_role()
    * Remove some unnecessary globals
    * Some coding standards issues

--- bp-groupblog-cssjs.php
+++ bp-groupblog-cssjs.php
@@ -9,14 +9,14 @@
 function bp_groupblog_add_admin_style() {
 
 	// add css
-	wp_enqueue_style('bp-groupblog-admin-style', plugins_url() . '/bp-groupblog/inc/css/admin.css');
-	wp_enqueue_style('jQueryUISmoothness', plugins_url() . '/bp-groupblog/inc/smoothness/jquery-ui-smoothness.css');
+	wp_enqueue_style( 'bp-groupblog-admin-style', plugins_url() . '/bp-groupblog/inc/css/admin.css' );
+	wp_enqueue_style( 'jQueryUISmoothness', plugins_url() . '/bp-groupblog/inc/smoothness/jquery-ui-smoothness.css' );
 
 	// add javascripts
 	wp_enqueue_script( 'bp-groupblog-admin-js', plugins_url() . '/bp-groupblog/inc/js/admin.js' );
-	wp_enqueue_script('jquery-ui-core');
-	wp_enqueue_script('jquery-ui-tabs');
-	wp_enqueue_script('effects.core', plugins_url() .'/bp-groupblog/inc/js/effects.core.js', array('jquery-ui-core'));
+	wp_enqueue_script( 'jquery-ui-core' );
+	wp_enqueue_script( 'jquery-ui-tabs' );
+	wp_enqueue_script( 'effects.core', plugins_url() . '/bp-groupblog/inc/js/effects.core.js', array( 'jquery-ui-core' ) );
 
 }
 
@@ -25,10 +25,11 @@ function bp_groupblog_add_admin_style() {
  */
 function bp_groupblog_add_js() {
 	if ( bp_is_groups_component() && bp_is_action_variable( 'group-blog' ) ) {
-		if ( file_exists( get_stylesheet_directory() . '/groupblog/js/general.js' ) )
+		if ( file_exists( get_stylesheet_directory() . '/groupblog/js/general.js' ) ) {
 			wp_enqueue_script( 'bp-groupblog-js', get_stylesheet_directory_uri() . '/groupblog/js/general.js', array( 'jquery' ) );
-		else
+		} else {
 			wp_enqueue_script( 'bp-groupblog-js', plugins_url() . '/bp-groupblog/groupblog/js/general.js', array( 'jquery' ) );
+		}
 	}
 }
 add_action( 'wp_enqueue_scripts', 'bp_groupblog_add_js', 1 );
@@ -38,12 +39,11 @@ add_action( 'wp_enqueue_scripts', 'bp_groupblog_add_js', 1 );
  */
 function bp_groupblog_add_screen_css() {
 	if ( bp_is_groups_component() ) {
-		if ( file_exists( get_stylesheet_directory() . '/groupblog/css/style.css' ) )
+		if ( file_exists( get_stylesheet_directory() . '/groupblog/css/style.css' ) ) {
 			wp_enqueue_style( 'bp-groupblog-screen', get_stylesheet_directory_uri() . '/groupblog/css/style.css' );
-		else
+		} else {
 			wp_enqueue_style( 'bp-groupblog-screen', plugins_url() . '/bp-groupblog/groupblog/css/style.css' );
+		}
 	}
 }
 add_action( 'wp_print_styles', 'bp_groupblog_add_screen_css' );
-
-?>
